d5f3526610bca6be12927e7318cd7faa8cc93a5d
ltlapy <lapy@lapy.link>
2024-04-26 17:12:00 +0900

    Roughly implement GET `meal/daily`

--- meals/urls.py
+++ meals/urls.py
@@ -10,6 +10,7 @@ router.register('liked', UserLikedMeals, basename='user_liked')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('daily/<date>', DailyMenuViewSet.as_view(), name='daily_menu'),
     # path('daily/<date>/<meal>/like', MealLikeViewSet.as_view({'get': 'create'}), name='daily_menu_like'),
     # path('liked', UserLikedMeals.as_view(), name='liked_menu'),
 ]
