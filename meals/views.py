2548f1ba9d19da9bd9b031d4a502da5965eed508
ltlapy <lapy@lapy.link>
2024-04-30 22:06:36 +0900

    meals/meal/ DELETE 구현 (wip)

--- meals/views.py
+++ meals/views.py
@@ -26,6 +26,13 @@ class UnitMenuViewSet(viewsets.ModelViewSet):
         res = queryset.filter(date__gte=since_date, date__lte=until_date)
         return res
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
 
 class DailyMenuViewSet(APIView):
     # queryset = DailyMenu.objects.all()
