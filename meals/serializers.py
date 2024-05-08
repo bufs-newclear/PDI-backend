d3bc367b737b5463c7b7a5439468faa77687c4db
ltlapy <lapy@lapy.link>
2024-05-08 18:00:26 +0900

    `/meals/meal`  인증 정보 제공 시 좋아요 정보 제공
    - `/meals/meal` 에 liked 필드 추가 (유저 인증시에만 제공)

--- meals/serializers.py
+++ meals/serializers.py
@@ -5,10 +5,26 @@ from .models import UnitMenu, UserMealLike
 
 
 class UnitMenuSerializer(serializers.ModelSerializer):
+    liked = serializers.SerializerMethodField()
+
     class Meta:
         model = UnitMenu
-        # fields = ['id', 'date', 'name', 'meal_type', 'like_count']
-        fields = '__all__'
+        fields = ['id', 'date', 'name', 'meal_type', 'like_count', 'liked']
+        # fields = '__all__'
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        request = self.context.get('request', None)
+        if request and not request.user.is_authenticated:
+            print('인증되지 않음; 좋아요 필드 배제')
+            ret.pop('liked', None)  # 사용자가 인증되지 않은 경우 liked 필드를 제거
+        return ret
+
+    def get_liked(self, obj):
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            return UserMealLike.objects.filter(user=user, meal=obj).exists()
+        return False
 
 
 class UserMealLikeSerializer(serializers.ModelSerializer):
